38f00963fd3bfb01cc47838fddd5120725c3a1c3
shenzq <shenzq@citycloud.com.cn>
2022-08-15 15:03:13 +0800

    feat(*): init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@cci/cp-theme-ant/lib/index.css'
 import './styles/index.scss'
 
 Vue.config.productionTip = false
